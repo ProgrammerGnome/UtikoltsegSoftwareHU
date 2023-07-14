a5ccc40dc24363e910c2cd8ac7a3d2801d00b9d1
Márk Király <69594158+ProgrammerGnome@users.noreply.github.com>
2023-07-14 23:42:58 +0200

    back UtikoltsegApplication.java

--- utikoltseg/src/main/java/com/example/utikoltseg/UtikoltsegApplication.java
+++ utikoltseg/src/main/java/com/example/utikoltseg/UtikoltsegApplication.java
@@ -3,8 +3,7 @@ package com.example.utikoltseg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class UtikoltsegApplication {
 
 	public static void main(String[] args) {
